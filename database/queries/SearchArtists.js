46e35f62982f90cd177183e576660de5b88052af
alemarnery <aleemar.95@gmail.com>
2021-05-17 14:22:49 -0430

    searchArtist

---
+++ database/queries/SearchArtists.js
@@ -0,0 +1,55 @@
+const Artist = require("../models/artist");
+
+/**
+ * Searches through the Artist collection
+ * @param {object} criteria An object with a name, age, and yearsActive
+ * @param {string} sortProperty The property to sort the results by
+ * @param {integer} offset How many records to skip in the result set
+ * @param {integer} limit How many records to return in the result set
+ * @return {promise} A promise that resolves with the artists, count, offset, and limit
+ */
+module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  //Write a query that will follow sort, offset, limit options only
+  //do not worry about 'criteria' yet
+
+  const query = Artist.find(buildQuery(criteria))
+    .sort({ [sortProperty]: 1 })
+    .skip(offset) //Se saltara n elementos
+    .limit(limit); // me va a traer elementos a partir de los q se salto
+
+  return Promise.all([query, Artist.find(buildQuery(criteria)).count()]).then(
+    (results) => {
+      return {
+        all: results[0],
+        count: results[1],
+        offset,
+        limit,
+      };
+    }
+  );
+};
+
+const buildQuery = (criteria) => {
+  const query = {};
+
+  if (criteria.name) {
+    //User.find({name: 'joe' })
+    query.$text = { $search: criteria.name };
+  }
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max,
+    };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max,
+    };
+  }
+
+  return query;
+};
