7815c4fe57d4ddb0b1bd7f8be0db06b5339e2edc
alemarnery <aleemar.95@gmail.com>
2021-05-24 14:22:49 -0430

    album

---
+++ database/models/album.js
@@ -0,0 +1,28 @@
+// Todo: create Album Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: {
+    type: String,
+  },
+  date: {
+    type: Date,
+  },
+  copiesSold: {
+    type: Number,
+  },
+  numberTracks: {
+    type: Number,
+  },
+  image: {
+    type: String,
+  },
+  revenue: {
+    type: Number,
+  },
+});
+
+//Model
+const Album = mongoose.model("album", AlbumSchema);
+module.exports = Album;
